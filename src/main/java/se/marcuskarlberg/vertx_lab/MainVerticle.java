591b58537f2223b05417ce95378f73a19efbb714
markar7 <marcus.karlberg@knowit.se>
2022-07-27 13:24:55 +0200

    added logger and handling errors on mainVerticle deployment

--- src/main/java/se/marcuskarlberg/vertx_lab/MainVerticle.java
+++ src/main/java/se/marcuskarlberg/vertx_lab/MainVerticle.java
@@ -2,8 +2,23 @@ package se.marcuskarlberg.vertx_lab;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MainVerticle extends AbstractVerticle {
+
+  private static final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
+
+  public static void main(String[] args) {
+    var vertx = Vertx.vertx();
+    vertx.exceptionHandler(error -> LOG.error("Unhandled: ", error));
+    vertx.deployVerticle(new MainVerticle())
+      .onFailure(e -> LOG.error("Failed to deploy main verticle: ", e))
+      .onSuccess(id -> {
+        LOG.info("Deployed {}, with id {}.", MainVerticle.class.getSimpleName(), id);
+      });
+  }
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
@@ -14,7 +29,7 @@ public class MainVerticle extends AbstractVerticle {
     }).listen(8888, http -> {
       if (http.succeeded()) {
         startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
+        LOG.info("HTTP Server started on port {}", 8888);
       } else {
         startPromise.fail(http.cause());
       }
